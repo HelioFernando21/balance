2b9f342c848aa7a14302e2349f199b558e12bde1
Helio Fernando <heliofernando21@gmail.com>
2025-06-08 11:50:37 -0300

    change TransactionRepository

--- src/main/java/com/bank/balance/repositories/TransactionRepository.java
+++ src/main/java/com/bank/balance/repositories/TransactionRepository.java
@@ -3,7 +3,6 @@ package com.bank.balance.repositories;
 import com.bank.balance.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
-public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
